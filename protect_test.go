43e97c07bad2cac2210c2bc1fe5f8b1e21251624
pauldurbin69 <pauldurbin@Pauls-MacBook-Pro.local>
2021-03-08 22:04:01 +0000

    Create protect_test.go

---
+++ protect_test.go
@@ -0,0 +1,13 @@
+package protect
+
+import "testing"
+
+func TestProtect(t *testing.T) {
+	got, err := Protect(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if got == nil {
+		t.Error("Got empty machine id")
+	}
+}
