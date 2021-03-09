90079adfe49c47330803e9b12697a825bf7628d9
pauldurbin69 <pauldurbin@Pauls-MacBook-Pro.local>
2021-03-09 17:51:54 +0000

    add save and load

--- protect.go
+++ protect.go
@@ -1,5 +1,10 @@
 package nativeprotect
 
+import (
+	"io/ioutil"
+	"os"
+)
+
 // Protect some stuff
 func Protect(clearBytes []byte) ([]byte, error) {
 	// Return a greeting that embeds the name in amessage := fmt.Sprintf("Hi, %v. Welcome!", name) message.
@@ -12,4 +17,46 @@ func Unprotect(cipherBytes []byte) ([]byte, error) {
 	// Return a greeting that embeds the name in amessage := fmt.Sprintf("Hi, %v. Welcome!", name) message.
 
 	return unprotectNative(cipherBytes)
+}
+
+// LoadSecret get encrypted data back from file in UserHomeDir()
+func LoadSecret(fileName string) ([]byte, error) {
+
+	path, err := getKeyFilePath(fileName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadFile(path)
+}
+
+// SaveSecret encrypted and save data to file in UserHomeDir() with r/w for user only
+func SaveSecret(fileName string, clearBytes []byte) error {
+
+	cipherBytes, err := protectNative(clearBytes)
+
+	if err != nil {
+		return err
+	}
+
+	path, err := getKeyFilePath(fileName)
+
+	if err != nil {
+		return err
+	}
+
+	// save to path user r/w only
+	return ioutil.WriteFile(path, cipherBytes, 0600)
+}
+
+func getKeyFilePath(fileName string) (string, error) {
+
+	home, err := os.UserHomeDir()
+
+	if err != nil {
+		return "", nil
+	}
+
+	return home + "/" + fileName, nil
 }
--- protect_test.go
+++ protect_test.go
@@ -3,11 +3,46 @@ package nativeprotect
 import "testing"
 
 func TestProtect(t *testing.T) {
-	got, err := Protect(nil)
+
+	wibble := "wibble"
+	got, err := Protect([]byte(wibble))
 	if err != nil {
 		t.Error(err)
 	}
 	if got == nil {
 		t.Error("Got empty machine id")
+	}
+
+	plainTextBack, err := Unprotect(got)
+
+	if string(plainTextBack) != wibble {
+		t.Error("incorrect decrypted value")
+	}
+}
+
+func TestSaveSecret(t *testing.T) {
+
+	err := SaveSecret("wibble-file", []byte("wibble"))
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLoadSecret(t *testing.T) {
+
+	wibble := "wibble"
+	err := SaveSecret("wibble-file", []byte(wibble))
+
+	cipherBytes, err := LoadSecret("wibble-file")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	plainTextBack, err := Unprotect(cipherBytes)
+
+	if string(plainTextBack) != wibble {
+		t.Error("incorrect decrypted value")
 	}
 }
